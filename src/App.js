95f26a1b0fea0aee56c83048c665eeb17a33bacf
Minilik Meja <minilik.meja@georgebrown.ca>
2025-10-05 12:38:40 -0400

    Updated student info (ID & name) and project structure

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: StudentID || Student Name || DevOps</h5>
+      <h5 style={{ color: 'blue' }}>Developed By: 100515804 || Minilik Meja || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
